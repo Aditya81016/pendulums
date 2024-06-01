6f6c03dda1c3fe95603fa015aea95f5ccbc58bcd
Aditya81016 <aditya.jaiswal.81016@gmail.com>
2024-06-01 21:00:46 +0530

    added more configurations

--- config.js
+++ config.js
@@ -1,18 +1,72 @@
 const params = window.location.search.substring(1).split("&");
 
+const isRandom = params.includes("random");
+
+const rand = (max, min = 0, precision = 3) =>
+  Number((min + Math.random() * (max - min)).toFixed(precision));
+const pickOne = (...args) => args[Math.floor(Math.random() * args.length)];
+const $ = (val1, val2) => (isRandom ? val2 : val1);
+
 const config = {
-  pendulums: 3,
-  speeds: undefined,
-  speed: Math.PI,
-  speedGap: 2,
-  height: 100,
-  heightGap: -100 / 4,
-  width: 4,
-  widthGap: 0,
-  angle: Math.PI / 2,
-  angleGap: 0,
-  lineWidth: 2,
+  pendulums: $(3, rand(5, 2, 0)),
+
+  speeds: $(undefined, pickOne(undefined, null)),
+  speed: $(Math.PI, Math.PI * pickOne(rand(-1, -5, 0), rand(5, 1, 0))),
+  speedGap: $(1, pickOne(rand(-1, -5, 0), rand(5, 1, 0))),
+
+  heights: $(undefined, pickOne(undefined, null)),
+  height: $(100, rand(100, 50)),
+  heightGap: $(-0.5, rand(0, -0.75)),
+
+  widths: undefined,
+  width: $(5, rand(10, 5)),
+  widthGap: $(-0.1, rand(0, -0.25)),
+
+  angles: $(undefined, pickOne(undefined, null)),
+  angle: $(Math.PI / 2, rand(Math.PI * 2, -Math.PI * 2)),
+  angleGap: $(Math.PI, rand(1, -1)),
+
+  saturation: $(85, rand(100)),
+  lightness: $(50, rand(100)),
+
+  colors: $(undefined, pickOne(undefined, null)),
+  color: $(360, rand(360)),
+  colorGap: $(0.5, rand(1, 0)),
+
+  lineWidth: $(2, rand(5, 1)),
+  lineColor: $("#fff", `hsl(${rand(360)} 85 85)`),
+  lineFill: $("#222", `hsl(${rand(360)} 50 10)`),
 };
+
+params.forEach((param) => {
+  const [key, value] = param.split("=");
+  if (value === "undefined") config[key] = undefined;
+  else
+    try {
+      config[key] = JSON.parse(value);
+    } catch {
+      if (typeof value === "string")
+        config[key] = value.replaceAll("%20", " ").replaceAll("%22", " ");
+      else config[key] = value;
+    }
+});
+
+["speeds", "heights", "angles", "colors"].forEach((key) => {
+  if (params.includes("random") && config[key] === null) {
+    config[key] = [];
+    for (let i = 0; i < config.pendulums; i++) {
+      const val = {
+        speeds: rand(Math.PI * 5, -Math.PI * 5, 0),
+        heights: rand(100, 50),
+        angles: rand(Math.PI * 1, -Math.PI * 1),
+        colors: `hsl(${rand(360)} 85 50)`,
+      }[key];
+      config[key].push(val);
+    }
+  }
+});
+
+console.log(config);
 
 const timer = {
   current: 0,
@@ -21,11 +75,6 @@ const timer = {
     return timer.skip / 1000;
   },
 };
-
-params.forEach((param) => {
-  const [key, value] = param.split("=");
-  config[key] = value ? JSON.parse(value) : value;
-});
 
 const svg = document.getElementById("svg");
 const pendulums = [];
--- main.js
+++ main.js
@@ -9,14 +9,28 @@ document.body.addEventListener("resize", () => {
   svg.height = window.innerHeight;
 });
 
+document.getElementById("nav").onclick = () => {
+  let params = "?";
+  for (const key in config)
+    params += key + "=" + JSON.stringify(config[key]) + "&";
+  window.location.assign(window.location.origin + params);
+};
+
+const $ = (base, gap, i) => base * (1 + gap) ** i;
+
 for (let i = 0; i < config.pendulums; i++) {
   pendulums.push(
     new Pendulum(
       i,
-      config.speeds ? config.speeds[i] : config.speed + config.speedGap * i,
-      config.height + config.heightGap * i,
-      config.width + config.widthGap * i,
-      config.angle + config.angleGap * i
+      config.speeds ? config.speeds[i] : $(config.speed, config.speedGap, i),
+      config.heights
+        ? config.heights[i]
+        : $(config.height, config.heightGap, i),
+      config.widths ? config.widths[i] : $(config.width, config.widthGap, i),
+      config.angles ? config.angles[i] : $(config.angle, config.angleGap, i),
+      config.colors
+        ? config.colors[i]
+        : `hsl(${$(config.color, config.colorGap, i)} 85 50)`
     )
   );
 }
